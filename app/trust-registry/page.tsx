590ceb3146c0cb73ffd39226d43bf4db8d1dfed8
Owooluwa Samson <godisovergods@gmail.com>
2025-11-12 09:12:06 +0100

    fix: correct syntax error in Trust Registry page rendering

--- app/trust-registry/page.tsx
+++ app/trust-registry/page.tsx
@@ -179,7 +179,7 @@ export default function TrustRegistryPage() {
                       </Badge>
                     </div>
                   </div>
-                ))}
+                ))};
               </div>
             )}
           </CardContent>
